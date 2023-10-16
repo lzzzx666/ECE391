e6f42e302efc5bd280c599ab8ea2b2402175fb58
hd2 <hd2@illinois.edu>
2023-10-15 22:03:53 -0500

    Update file systemcall.h

--- student-distrib/systemcall.h
+++ student-distrib/systemcall.h
@@ -1,6 +1,7 @@
 #ifndef SYSTEMCALL
 #define SYSTEMCALL
 #include "types.h"
+#include "lib.h"
 extern int32_t sys_halt(uint8_t status);
 extern int32_t sys_execute(const uint8_t *command);
 extern int32_t sys_read(int32_t fd, void *buf, int32_t nbytes);
