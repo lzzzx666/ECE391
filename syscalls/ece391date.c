b1363d322686ef2bb5d2934c35bcd2d3eb08c97a
Zifan Ying <zifany4@illinois.edu>
2023-12-07 04:22:00 -0600

    enable color; improve date

--- syscalls/ece391date.c
+++ syscalls/ece391date.c
@@ -5,21 +5,13 @@
 #include "time.h"
 
 const uint8_t* day_of_week[] = {0, "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-const uint32_t days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int main() {
     int32_t fd = ece391_open("rtc");
     if (fd == -1) return 1;
     time_t time;
+    time.Timezone = TIMEZONE;
     if (ece391_ioctl(fd, GET_TIME_CTL, &time)) return 2;
-
-    time.Hours += TIMEZONE;
-    if (time.Hours < 0) time.Day_of_Month--;
-    if (time.Hours >= 24) time.Day_of_Month++;
-    if (time.Day_of_Month < 1) time.Month--;
-    if (time.Day_of_Month > days_in_month[time.Month]) time.Month++;
-    if (time.Month < 1) time.Year--;
-    if (time.Month > 12) time.Year++;
 
     ece391_printf("20%d %d/%d %s %d:%d:%d UTC%s%d\n", time.Year, time.Month,
                   time.Day_of_Month, day_of_week[time.Weekday], time.Hours, time.Minutes,
